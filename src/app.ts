e9f5d073d08c24a9ac46ccbf37168f8cd441298e
Fulal-kt <muhamedfulail77@gmail.com>
2023-11-27 16:03:13 +0530

    env update

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,8 @@
 import express from 'express';
 import userRoute from './frameworks/routes/userRouter';
 import { DbConnect } from './frameworks/config/DbConnet'
-import session from 'express-session';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import crypto from 'crypto'; // Import the crypto module
 require('dotenv').config();
 
 const app = express();
@@ -14,20 +12,12 @@ const corsOptions = {
   origin: true,
 };
 
-// Generate a secure random string for the session secret
-const secret = crypto.randomBytes(64).toString('hex');
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(
-  session({
-    secret: secret,
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false },
-  })
-);
+
 app.use(cors(corsOptions));
 
 app.use('/api/v1/', userRoute);
